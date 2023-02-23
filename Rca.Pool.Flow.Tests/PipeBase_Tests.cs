56e58c1ee52a78477045bf2981833b12f98c0ae4
Elias Ruemmler <46575176+100prznt@users.noreply.github.com>
2023-02-23 19:42:23 +0100

    bring it to life

---
+++ Rca.Pool.Flow.Tests/PipeBase_Tests.cs
@@ -0,0 +1,29 @@
+using Rca.Physical;
+using System.Net.NetworkInformation;
+
+namespace Rca.Pool.Flow.Tests
+{
+    [TestClass]
+    public class PipeBase_Tests
+    {
+        readonly PhysicalValue Diameter = new(45.2, PhysicalUnits.Millimetre);
+
+        [TestMethod]
+        public void CalcFlowVelocity_Test()
+        {
+            PipeBase testPipe = new (Diameter);
+
+            // Vergleichsergebnis aus Druckverlust 7.0 (http://druckverlust.de)
+            Assert.AreEqual(0.866, testPipe.CalcFlowVelocity(new PhysicalValue(5, PhysicalUnits.CubicMetrePerHour)).ValueAs(PhysicalUnits.MetrePerSecond), 1E-3);
+        }
+
+        [TestMethod]
+        public void CalcFlowRate_Test()
+        {
+            PipeBase testPipe = new(Diameter);
+
+            // Vergleichsergebnis aus Druckverlust 7.0 (http://druckverlust.de)
+            Assert.AreEqual(10, testPipe.CalcFlowRate(new PhysicalValue(1.731, PhysicalUnits.MetrePerSecond)).ValueAs(PhysicalUnits.CubicMetrePerHour), 1E-3);
+        }
+    }
+}
---
+++ Rca.Pool.Flow/PipeBase.cs
@@ -0,0 +1,105 @@
+﻿using Rca.Physical;
+using Rca.Physical.Dimensions;
+
+namespace Rca.Pool.Flow
+{
+    public class PipeBase
+    {
+        #region Properties
+        /// <summary>
+        /// Innerer Rohrdurchmesser
+        /// </summary>
+        public PhysicalValue Diameter { get; set; }
+
+        /// <summary>
+        /// Rohrlänge
+        /// </summary>
+        public PhysicalValue Length { get; set; }
+
+        /// <summary>
+        /// Rohrquerschnitt
+        /// </summary>
+        public PhysicalValue CrossArea
+        {
+            get
+            {
+                return new PhysicalValue(Math.PI * Math.Pow(Diameter.GetBaseValue(), 2) / 4, PhysicalDimensions.Area.GetBaseUnit());
+            }
+        }
+
+        /// <summary>
+        /// EINGABE PARAMETER
+        /// Volumenstrom
+        /// </summary>
+        public PhysicalValue FlowRate { get; set; }
+
+        #endregion Properties
+
+        #region Constructor
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public PipeBase()
+        {
+
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="di">Innerer Rohrdurchmesser</param>
+        public PipeBase(PhysicalValue di)
+        {
+            Diameter = di;
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="l">Rohrlänge</param>
+        /// <param name="di">Innerer Rohrdurchmesser</param>
+        public PipeBase(PhysicalValue di, PhysicalValue l)
+        {
+            Length = l;
+            Diameter = di;
+        }
+
+        #endregion Constructor
+
+        #region Services
+        /// <summary>
+        /// Strömungsgeschwindigkeit berechnen
+        /// </summary>
+        /// <returns>Strömungsgeschwindigkeit in [m/s]</returns>
+        public PhysicalValue CalcFlowVelocity() => CalcFlowVelocity(FlowRate);
+
+        /// <summary>
+        /// Strömungsgeschwindigkeit berechnen
+        /// </summary>
+        /// <param name="flowRate">Volumenstrom in [m^3/h]</param>
+        /// <returns>Strömungsgeschwindigkeit in [m/s]</returns>
+        public PhysicalValue CalcFlowVelocity(PhysicalValue flowRate)
+        {
+            //double q = flowRate / 3600; // [m^3/s]
+            //double a = CrossArea / 1E6; // [m^2]
+
+            return flowRate / CrossArea;
+        }
+
+        /// <summary>
+        /// Volumenstrom berechnen
+        /// </summary>
+        /// <param name="flowVelocity">Strömungsgeschwindigkeit in [m/s]</param>
+        /// <returns>Volumenstrom in [m^3/h]</returns>
+        public PhysicalValue CalcFlowRate(PhysicalValue flowVelocity)
+        {
+            //double a = CrossArea / 1E6; // [m^2]
+            //double q = ; // [m^3/s]
+
+            return flowVelocity * CrossArea;
+        }
+
+        #endregion Services
+    }
+
+}
